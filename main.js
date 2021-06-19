b974e18fb6ad14f2ca7d684046987c03e1d3ddb4
Sanya134 <sanyaparajuli123@gmail.com>
2021-06-19 09:49:25 +0545

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+setInterval(displayclock,1000);
+function displayclock(){
+  var time =new Date();
+  var hrs = time.getHours();
+  var min = time.getMinutes();
+  var sec = time.getSeconds();
+  var am = "AM";
+
+  if(hrs> 12){
+    am = 'PM';
+  }
+
+  if(hrs>12){
+    hrs-=12;
+  }
+  if (hrs==0){
+    hrs=12;
+  }
+  if (hrs<10){
+    hrs='0'+hrs;
+  }
+  if (min<10){
+    min='0'+min;
+  }
+  if (sec<10){
+    sec='0'+sec;
+  }
+  document.getElementById('time').innerHTML=hrs+':'+min+':'+sec+' '+am;
+
+}
